e91f0fd04ad455e12045ec16a78e7e1988a65fb1
Lewis Wilson <turtleclam25@gmail.com>
2026-03-27 04:15:48 -0400

    add kaggle dataset

--- spotify_collector.py
+++ spotify_collector.py
@@ -10,30 +10,30 @@ headers = {"Authorization": f"Bearer {token}"}
 songs = []
 current_page = 0
 
-while len(songs) < 100:
-    response = requests.get("https://api.spotify.com/v1/search", headers=headers, params={"q": "year:2000-2024", "type": "track", "limit": 10, "offset": current_page}).json()
-    songSet = response.get("tracks", {}).get("items", [])
-    print(songSet[0].keys())
-    if not songSet: break
-    filteredSongs = []
-    for track in songSet:
-        song = {
-            "name": track.get("name"),
-            "popularity": track.get("popularity"),
-            "releaseYear": track.get("album", {}).get("release_date", "")[:4],
-            "length": track.get("duration_ms"),
-            "artist": track.get("artists", [{}])[0].get("name")
-        }
-        filteredSongs.append(song)
+while len(songs) < 1000:
+    response = requests.get("https://api.spotify.com/v1/search", headers=headers, params={"q": "year:2000-2024", "type": "track", "limit": 10, "offset": current_page}).status_code
+    print(response)
+#     songSet = response.get("tracks", {}).get("items", [])
 
-    songs.extend(filteredSongs)
-    current_page += 10
-    time.sleep(0.01)
-    #print(songSet)
+#     if not songSet: break
+#     filteredSongs = []
+#     for track in songSet:
+#         song = {
+#             "name": track.get("name"),
+#             "releaseYear": track.get("album", {}).get("release_date", "")[:4],
+#             "length": track.get("duration_ms"),
+#             "artist": track.get("artists", [{}])[0].get("name")
+#         }
+#         filteredSongs.append(song)
 
-print(songs[0])
-print("Hello, World!")
-with open("songs.csv", "w", newline="", encoding="utf-8") as file:
-    writer = csv.DictWriter(file, fieldnames=["name", "popularity", "releaseYear", "length", "artist"])
-    writer.writeheader()
-    writer.writerows(songs)
+#     songs.extend(filteredSongs)
+#     current_page += 10
+#     time.sleep(0.01)
+#     #print(songSet)
+
+# print(songs[0])
+# print("Hello, World!")
+# with open("songs.csv", "w", newline="", encoding="utf-8") as file:
+#     writer = csv.DictWriter(file, fieldnames=["name", "releaseYear", "length", "artist"])
+#     writer.writeheader()
+#     writer.writerows(songs)
